0096005df099be14100df735e15b53b22320719a
rex black <rexnblack@gmail.com>
2020-06-30 22:12:49 -0500

    added ugly main menu

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public string startScene;
+    public string settingsScene;
+
+    public void StartGame() 
+    {
+        SceneManager.LoadScene(startScene);
+    }
+
+    public void settings() 
+    {
+        SceneManager.LoadScene(settingsScene);
+    }
+}
